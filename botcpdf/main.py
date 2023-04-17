753442bbab23e6ee23fec343d30a2c4ada26f15c
Chisel <chisel@malik-wright.uk>
2023-04-17 20:00:47 +0100

    add support for fetching remote http json script data
    - light on error handling
    - not sure how to handle scripts without meta>name

--- botcpdf/main.py
+++ botcpdf/main.py
@@ -4,22 +4,39 @@ import sys
 
 from botcpdf.script import Script
 
-from botcpdf.util import load_data
+from botcpdf.util import fetch_remote_data, load_data
 
 
 def main():
     """Main entry point for the botcpdf package."""
     # default scriptname - change if just running
-    # if using a commandline, specify the name as the first arg
-    filename = "Trouble Brewing.json"
-    if len(sys.argv) > 1:
+
+    # if we don't have any args, exit with an error
+    if len(sys.argv) == 1:
+        print("Usage: botcpdf <scriptname>|<scriptjsonurl>")
+        exit(1)
+
+    # if our first arg starts with http, assume it's a URL
+    if sys.argv[1].startswith("http"):
+        script_name = "Unnamed Remote Script"
+        print(f"Fetching remote script data from {sys.argv[1]}…")
+        script_data = fetch_remote_data(sys.argv[1])
+        # script_data should be a list of dicts
+        # if there's an entry with id: meta, and a name, use that
+        if isinstance(script_data, list):
+            for entry in script_data:
+                if entry.get("id") == "_meta" and entry.get("name"):
+                    script_name = entry["name"]
+                    break
+
+    # otherwise, assume it's a filename
+    else:
         filename = sys.argv[1]
+        # this is kinda rudimentary, but it works as a first pass
+        # strip the path and extension from the filename
+        script_name = filename.rsplit("/", maxsplit=1)[-1].split(".", maxsplit=1)[0]
 
-    script_data = load_data(filename)
-
-    # this is kinda rudimentary, but it works as a first pass
-    # strip the path and extension from the filename
-    script_name = filename.rsplit("/", maxsplit=1)[-1].split(".", maxsplit=1)[0]
+        script_data = load_data(filename)
 
     script = Script(script_name, script_data)
     print(f"""Rendering "{script.title}"…""")
