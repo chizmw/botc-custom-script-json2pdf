6a7f4eb1b049aff5d9c9955c2ed9a8eeaf217af9
Chisel <chisel@malik-wright.uk>
2023-04-22 01:15:22 +0100

    add provider.aws.default_tags

--- terraform/_config.tf
+++ terraform/_config.tf
@@ -17,4 +17,7 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
+  default_tags {
+    tags = local.default_tags
+  }
 }
---
+++ terraform/_locals.tf
@@ -0,0 +1,10 @@
+locals {
+
+  default_tags = {
+    Name    = "botc-custom-script-json2pdf"
+    Owner   = "chisel"
+    Product = "botc-custom-script-json2pdf"
+    InfoURL = "https://github.com/chizmw/botc-custom-script-json2pdf"
+  }
+
+}
