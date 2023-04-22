1cb3dcbf24ff02ae72d1086e5125bf687b65956a
Chisel <chisel@malik-wright.uk>
2023-04-22 23:39:27 +0100

    add terraform for pdf s3 bucket

---
+++ terraform/s3.tf
@@ -0,0 +1,34 @@
+# create an s3 bucket for the generated PDF files
+resource "aws_s3_bucket" "botc_pdf_bucket" {
+  bucket   = "436158765452-botc-pdf-bucket"
+  provider = aws
+  tags = merge(
+    local.default_tags,
+    {
+      Name = "botc-pdf-bucket"
+    }
+  )
+}
+
+resource "aws_s3_bucket_ownership_controls" "botc_pdf_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.botc_pdf_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+
+resource "aws_s3_bucket_acl" "botc_pdf_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.botc_pdf_bucket_ownership_controls
+  ]
+  bucket = aws_s3_bucket.botc_pdf_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "botc_pdf_bucket_versioning" {
+  bucket = aws_s3_bucket.botc_pdf_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
