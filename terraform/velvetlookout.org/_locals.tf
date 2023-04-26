878eae64a2cdf76cb37b0ed9d00e746fafb5d54d
Chisel <chisel@malik-wright.uk>
2023-04-26 11:17:40 +0100

    add terraform management of velvetlookout assets

---
+++ terraform/velvetlookout.org/_locals.tf
@@ -0,0 +1,10 @@
+locals {
+
+  tag_defaults = {
+    Name    = "botc-custom-script-json2pdf"
+    Owner   = "chisel"
+    Product = "botc-custom-script-json2pdf"
+    InfoURL = "https://github.com/chizmw/botc-custom-script-json2pdf"
+  }
+
+}
---
+++ terraform/velvetlookout.org/_vars.tf
@@ -0,0 +1,5 @@
+variable "domain_name" {
+  description = "The domain name for the website."
+  type        = string
+  default     = "velvetlookout.org"
+}
