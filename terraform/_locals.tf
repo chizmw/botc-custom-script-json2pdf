d900dcabdc4d6c1f2bfb1af1280b194ba50365e0
Chisel <chisel@malik-wright.uk>
2023-05-01 20:27:38 +0100

    add workspace enabled s3 web support

--- terraform/_locals.tf
+++ terraform/_locals.tf
@@ -6,4 +6,12 @@ locals {
     InfoURL = "https://github.com/chizmw/botc-custom-script-json2pdf"
   }
 
+  # wkspc_site_name is based on the workspace name; if it's 'prod' we use 'make', if it's 'dev' we use 'preview'
+  wkspc_site_name = {
+    prod = "maker"
+    dev  = "preview"
+  }
+
+  # get the lookup value from the wkspc_site_name map based on the workspace name
+  site_name = lookup(local.wkspc_site_name, terraform.workspace)
 }
