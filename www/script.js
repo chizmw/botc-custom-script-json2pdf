04e8ebf41f1d454d69958624762836b4c1ffb9db
Chisel <chisel@malik-wright.uk>
2023-05-09 00:25:06 +0100

    update API_KEY (yeah, I know)

--- www/script.js
+++ www/script.js
@@ -1,4 +1,4 @@
-const API_KEY = 'BpMKPlqwE01o9At5ldhB33QDjKgxmGHs4XhZhCmZ';
+const API_KEY = 'plTBNe7lT95DmuPMyBm2w8bK4cMbwIQk4ci547Kc';
 
 Dropzone.options.customScript = {
   url: API_GATEWAY_URL + '/render',
