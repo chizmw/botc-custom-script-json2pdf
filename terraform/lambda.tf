53c24863d5ecb6394e20ee026ae78fcc176dfe4b
Chisel <chisel@malik-wright.uk>
2023-05-09 16:38:59 +0100

    remove lambda_invalidate_cache (now in infra-arcane-scripts.net)

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,19 +1,22 @@
-# Declare the local zip archive
-data "archive_file" "lambda_code" {
-  type        = "zip"
-  source_file = "./lambda-src/invalidate_cache.py"
-  output_path = "./tmp/invalidatecache.zip"
-}
+# lambda itself should be managed by
+# infra-arcane-scripts.net/terraform
 
-data "aws_iam_role" "iam_for_lambda" {
-  name = "deploy_json2pdf"
-}
+# # Declare the local zip archive
+# data "archive_file" "lambda_code" {
+#   type        = "zip"
+#   source_file = "./lambda-src/invalidate_cache.py"
+#   output_path = "./tmp/invalidatecache.zip"
+# }
 
-resource "aws_lambda_function" "lambda_invalidate_cache" {
-  function_name    = "invalidate-cache"
-  filename         = data.archive_file.lambda_code.output_path
-  source_code_hash = data.archive_file.lambda_code.output_base64sha256
-  role             = data.aws_iam_role.iam_for_lambda.arn
-  handler          = "invalidate_cache.lambda_handler"
-  runtime          = "python3.10"
-}
+# data "aws_iam_role" "iam_for_lambda" {
+#   name = "deploy_json2pdf"
+# }
+
+# resource "aws_lambda_function" "lambda_invalidate_cache" {
+#   function_name    = "invalidate-cache"
+#   filename         = data.archive_file.lambda_code.output_path
+#   source_code_hash = data.archive_file.lambda_code.output_base64sha256
+#   role             = data.aws_iam_role.iam_for_lambda.arn
+#   handler          = "invalidate_cache.lambda_handler"
+#   runtime          = "python3.10"
+# }
