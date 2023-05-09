65c7281463878c793aded3e885c576052985ce9f
Chisel <chisel@malik-wright.uk>
2023-05-09 10:55:03 +0100

    fixup Access-Control-Allow-Origin in main.py

--- terraform/main.py
+++ terraform/main.py
@@ -17,7 +17,8 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(some_response),
         "headers": {
-            "Access-Control-Allow-Origin": "http://127.0.0.1:5500",
+            # "Access-Control-Allow-Origin": "http://127.0.0.1:5500",
+            "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "Content-Type,access-control-allow-origin,X-Chisel-Info",
         },
     }
