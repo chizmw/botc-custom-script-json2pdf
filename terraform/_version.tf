eab4aaff3fd1ea27c30ae7598bde0e5a02a24784
Chisel <chisel@malik-wright.uk>
2023-07-18 08:59:32 +0100

    NUKE EVERYTHING!

--- terraform/_version.tf
+++
@@ -1,8 +0,0 @@
-
-data "external" "useful_version_info" {
-  program = ["bash", "${path.module}/get-useful-information.sh"]
-}
-
-output "useful_version_info" {
-  value = data.external.useful_version_info.result
-}
