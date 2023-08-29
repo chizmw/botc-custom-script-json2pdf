1a9ecbb1ad095cdd2e39dc61d9ea271d92b6a5d4
Chisel <chisel@malik-wright.uk>
2023-08-29 22:29:37 +0100

    docs(changeset): chore: be explicit about unexpected keys in script metadata on init

---
+++ .changeset/chilly-beds-admire.md
@@ -0,0 +1,5 @@
+---
+'arcane-scripts': patch
+---
+
+chore: be explicit about unexpected keys in script metadata on init
--- botcpdf/script.py
+++ botcpdf/script.py
@@ -42,7 +42,7 @@ class ScriptMeta:
         if not set(data.keys()).issubset({"id", "name", "author", "logo"}):
             # get the keys that are not in the list of known keys
             unexpected_keys = set(data.keys()) - {"id", "name", "author", "logo"}
-            raise ValueError("Unexpected fields in script metadata: " + unexpected_keys)
+            raise ValueError(f"Unexpected fields in script metadata: {unexpected_keys}")
 
         self.name = data.get("name", None)
         self.author = data.get("author", None)
